57e12be6c630dc140cb0ad851c9aa044cbe63d98
Student: Mahdi Mahdi <94849166+MahdiAljaberi2@users.noreply.github.com>
2022-02-01 14:27:01 +0000

    added traversals of ArrayLists

--- Main.java
+++ Main.java
@@ -15,8 +15,8 @@ class Main {
     System.out.println("String removed: "+lastNames.remove(2));
     System.out.println(lastNames);
     System.out.println("Size of lastNames is "+lastNames.size());
-    System.out.println("Removed String using set is "+lastNames.set(0, "Eanderson"));
-    lastNames.clear();
+    System.out.println("Removed String using set is "+lastNames.set(0, "Earlheim"));
+    //lastNames.clear();
     System.out.println(lastNames);
     System.out.println("Size of lastNames is "+lastNames.size());
     Integer one=new Integer(1);
@@ -31,6 +31,46 @@ class Main {
     demo.add(three);
     System.out.println(demo);
 
+    //Traversals start here (Lesson 2)
+
+    for (int i=0;i<lastNames.size();i++)
+    {
+      System.out.println("Item "+(i+1)+": "+lastNames.get(i));
+    }
+
+for (int i=lastNames.size()-1;i>=0;i--)
+    {
+      System.out.println("Item "+(i+1)+": "+lastNames.get(i));
+    }
+
+lastNames.add("Francis");
+lastNames.add("Gorsline");
+lastNames.add("Hearthshire");
+
+for (String x:lastNames)
+{
+  System.out.println(x);
+}
+
+ArrayList<String>letters=new ArrayList<String>();
+
+letters.add("A");
+letters.add("B");
+letters.add("C");
+letters.add("D");
+letters.add("E");
+letters.add("F");
+
+for(int i=0;i<letters.size();i++)
+{
+  for (int j=0;j<lastNames.size();j++)
+  {
+    if (letters.get(i).equals(lastNames.get(j).substring(0,1)))
+    {
+      System.out.println(lastNames.get(j)+" starts with "+letters.get(i));
+    }
+  }
+}
 
   }
 }
